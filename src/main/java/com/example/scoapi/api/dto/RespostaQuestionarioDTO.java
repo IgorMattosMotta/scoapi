d527d965b5ceae59a2eb04ae8484977b32de9fc7
Igor Mattos <igormmattos06@gmail.com>
2026-05-22 10:01:47 -0300

    Refatora: Ajusta meétodos de criação em RespostaQuestionarioDTO e SolicitacaoadocaoDTO

--- src/main/java/com/example/scoapi/api/dto/RespostaQuestionarioDTO.java
+++ src/main/java/com/example/scoapi/api/dto/RespostaQuestionarioDTO.java
@@ -1,15 +1,11 @@
 package com.example.scoapi.api.dto;
 
-import com.example.scoapi.model.entity.Animal;
-import com.example.scoapi.model.entity.ONG;
-import com.example.scoapi.model.entity.RegistroVacina;
 import com.example.scoapi.model.entity.RespostaQuestionario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
-import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,15 +17,15 @@ public class RespostaQuestionarioDTO {
     private Long idPergunta;
     private String textoPergunta;
 
-    public static RespostaQuestionario create(RespostaQuestionario respostaQuestionario) {
+    public static RespostaQuestionarioDTO create(RespostaQuestionario respostaQuestionario) {
         ModelMapper modelMapper = new ModelMapper();
-        RespostaQuestionario dto = modelMapper.map(respostaQuestionario, RespostaQuestionario.class);
-        if(respostaQuestionario.getSolicitacao() != null){
+        RespostaQuestionarioDTO dto = modelMapper.map(respostaQuestionario, RespostaQuestionarioDTO.class);
+        if (respostaQuestionario.getSolicitacao() != null) {
             dto.idSolicitacao = respostaQuestionario.getSolicitacao().getId();
         }
-        if(respostaQuestionario.getPergunta() != null){
+        if (respostaQuestionario.getPergunta() != null) {
             dto.idPergunta = respostaQuestionario.getPergunta().getId();
-            dto.textoPergunta = respostaQuestionario.getPergunta().getId();
+            dto.textoPergunta = respostaQuestionario.getPergunta().getTexto();
         }
         return dto;
     }
--- src/main/java/com/example/scoapi/api/dto/SolicitacaoadocaoDTO.java
+++ src/main/java/com/example/scoapi/api/dto/SolicitacaoadocaoDTO.java
@@ -1,5 +1,6 @@
 package com.example.scoapi.api.dto;
 
+import com.example.scoapi.model.entity.SolicitacaoAdocao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,12 +23,21 @@ public class SolicitacaoadocaoDTO {
     private Long idStatus;
     private String statusDescricao;
 
-    public static SolicitacaoadocaoDTO create( SolicitacaoadocaoDTO solicitacao){
+    public static SolicitacaoadocaoDTO create(SolicitacaoAdocao solicitacao) {
         ModelMapper modelMapper = new ModelMapper();
         SolicitacaoadocaoDTO dto = modelMapper.map(solicitacao, SolicitacaoadocaoDTO.class);
 
-        if(solicitacao.getIdAdotante() != null){
-            dto.nomeAdotante = solicitacao.getNomeAdotante();
+        if (solicitacao.getAdotante() != null) {
+            dto.idAdotante = solicitacao.getAdotante().getId();
+            dto.nomeAdotante = solicitacao.getAdotante().getNomeCompleto();
+        }
+        if (solicitacao.getAnimal() != null) {
+            dto.IdAnimal = solicitacao.getAnimal().getId();
+            dto.nomeAnimal = solicitacao.getAnimal().getNome();
+        }
+        if (solicitacao.getStatus() != null) {
+            dto.idStatus = solicitacao.getStatus().getId();
+            dto.statusDescricao = solicitacao.getStatus().getStatus();
         }
         return dto;
     }
